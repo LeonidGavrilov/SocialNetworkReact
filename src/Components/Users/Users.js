db9c5608a8666a6c02cd0dcfc89f760a9e4b17fd
LeonidGavrilov <leo2000--10@mail.ru>
2021-02-18 21:06:50 +0300

    userAPI

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,16 +1,14 @@
 import React from "react";
 import styles from "./users.module.css";
+import axios from "axios";
+import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
     if (props.users.length === 0) {
-    props.setUsers(
-        [
-            {id: 1, photoURL: 'https://www.media2.hw-static.com/wp-content/uploads/58769132.jpg', followed: false, fullName: 'Dmitry', status: 'bla bla', location: {city: 'Moscow', country: 'Russia'}},
-            {id: 2, photoURL: 'https://media.todaybirthdays.com/2015/11/10/jake-pitts0.jpg', followed: false, fullName: 'Vadim', status: 'bla bla', location: {city: 'Minsc', country: 'Belarus'}},
-            {id: 3, photoURL: 'https://i.pinimg.com/originals/de/84/0a/de840ac63767cbc461e0f10d7c239d69.jpg', followed: true, fullName: 'Sara', status: 'bla bla', location: {city: 'Tel-Aviv', country: 'Israel'}},
-            {id: 4, photoURL: 'https://avatars.mds.yandex.net/get-zen_doc/1567788/pub_5d610858bd45c000ad9d4df8_5d610a67c49f2900ad3a0b76/scale_1200', followed: true, fullName: 'Ameliy', status: 'bla bla', location: {city: 'New York', country: 'USA'}},
-        ]
-    )
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setUsers(response.data.items);
+            });
     }
     return (
         <div>
@@ -18,7 +16,8 @@ let Users = (props) => {
                 props.users.map(u =>
                     <div key={u.id}>
                         <span>
-                            <div><img src={u.photoURL} className={styles.userPhoto}/></div>
+                            <div><img src={u.photos.small != null ? u.photos.small : userPhoto}
+                                      className={styles.userPhoto}/></div>
                             <div>
                                 {u.followed
                                     ? <button onClick={() => (props.unfollow(u.id))}>Unfollow</button>
@@ -27,13 +26,13 @@ let Users = (props) => {
                             </div>
                         </span>
                         <span>
-                            <span>
-                                <div>{u.fullName}</div>
+                            <span>s
+                                <div>{u.name}</div>
                                 <div>{u.status}</div>
                             </span>
                             <span>
-                                <div>{u.location.country}</div>
-                                <div>{u.location.city}</div>
+                                <div>{"u.location.country"}</div>
+                                <div>{"u.location.city"}</div>
                             </span>
                         </span>
                     </div>)
