1c9ec97c8cd193da079448f7dec2e1f8fe988acc
LeonidGavrilov <leo2000--10@mail.ru>
2021-02-25 02:47:25 +0300

    Презентационная и Контейнерная компоненты(56)

--- src/Components/Users/UsersContainer.js
+++ src/Components/Users/UsersContainer.js
@@ -1,7 +1,43 @@
 import React from "react";
-import Users from "./Users";
+import axios from "axios";
 import {connect} from "react-redux";
 import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC} from "../../Redux/users-reducer";
+import Users from "./Users";
+
+
+class UsersContainer extends React.Component {
+    componentDidMount() {
+        // axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        axios.get(`http://localhost:3001/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                console.log(response);
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        // axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        axios.get(`http://localhost:3001/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                console.log(response);
+                this.props.setUsers(response.data.items);
+            });
+    }
+
+    render() {
+        return (
+            <Users totalUsersCount={this.props.totalUsersCount}
+                   pageSize={this.props.pageSize}
+                   currentPage={this.props.currentPage}
+                   onPageChanged={this.onPageChanged}
+                   users={this.props.users}
+                   follow={this.props.follow}
+                   unfollow={this.props.unfollow}/>
+        );
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
@@ -31,6 +67,4 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
-
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
